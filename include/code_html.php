418659641ec5aa26efcee5c5214975de6430aba0
Arnaud69 <arnaud.gueneley@gmail.com>
2014-11-03 23:55:37 +0100

    add popup for sub
    add code for popup when subscribe

--- include/code_html.php
+++ include/code_html.php
@@ -6,8 +6,17 @@ if(isset($list_id)&&!empty($list_id)) {
     $newsletter=getConfig($cnx,$list_id,$row_config_globale['table_listsconfig']);
 	echo "<p>Copiez ce code et insérez le dans la ou les pages de votre portail, pour que vos utilisateurs puissent souscrire à vos lettres d'information.</p>
 	<fieldset>
-	<label>Code :</label>
-<textarea cols='70%' rows='6'><form action='/".$row_config_globale['path']."subscription.php' method='post' target='_blank'>
+	<label>Code avec pop-up :</label>
+<textarea cols='70%' rows='8'><form action='/".$row_config_globale['path']."subscription.php' method='post' target='pmnlwindow' onsubmit=\"window.open('/".$row_config_globale['path']."subscription.php', 'pmnlwindow', 'scrollbars=yes,width=700,height=210');return true\">
+<input type='text' name='email_addr' value='' size='30'>
+<input type='hidden' name='list_id' value='$list_id'>
+<input type='hidden' name='op' value='join'>
+<input type='submit' value='souscrire'>
+</form></textarea>
+	</fieldset>
+	<fieldset>
+	<label>Code en page pleine :</label>
+<textarea cols='70%' rows='8'><form action='/".$row_config_globale['path']."subscription.php' method='post' target='_blank'>
 <input type='text' name='email_addr' value='' size='30'>
 <input type='hidden' name='list_id' value='$list_id'>
 <input type='hidden' name='op' value='join'>
