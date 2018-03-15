56bf12ffbf3cab282b69f287a53ae4f04e17f089
Arnaud69 <arnaud.gueneley@gmail.com>
2018-03-15 12:41:11 +0100

    Delete all the old stuff, push the full new 2.0.5

--- include/wysiwyg.php
+++
@@ -1,4 +0,0 @@
-<iframe src="include/editeur.php?token=<?php 
-echo $token;?>&list_id=<?php 
-echo $list_id;?>" width="98%" style="margin:0;padding:0;border:none;padding-left:8px;margin-bottom:30px;height:700px;"><?php 
-echo tr("IFRAME_NOT_SUPPORTED"); ?></iframe>
